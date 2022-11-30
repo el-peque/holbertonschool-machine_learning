8628e7335f8746018a9a52394da429d15d0212e6
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-11-30 00:34:04 -0300

    Update task 2

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-def matrix_shape(matrix):
-    shape = []
-    if matrix and isinstance(matrix, list):
-        matrix_cpy = matrix.copy()
-        shape.append(len(matrix_cpy))
+if __name__ == "__main__":
+    def matrix_shape(matrix):
+        shape = []
+        if matrix and isinstance(matrix, list):
+            matrix_cpy = matrix.copy()
+            shape.append(len(matrix_cpy))
 
-    while isinstance(matrix_cpy[0], list):
-        matrix_cpy = matrix_cpy[0]
-        shape.append(len(matrix_cpy))
+        while isinstance(matrix_cpy[0], list):
+            matrix_cpy = matrix_cpy[0]
+            shape.append(len(matrix_cpy))
 
-    return shape
+        return shape
