fe193e96cfcd7282bcd283060aa62f19771591d5
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-11-30 15:29:15 -0300

    Fix task 8

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""mat_mul"""
+"""Matrix multiplication"""
 
 
 def mat_mul(mat1, mat2):
     """Performs matrix multiplication"""
     if len(mat1[0]) != len(mat2):
         return None
-
     res = []
-
     for i in range(len(mat1)):
         row = []
         for j in range(len(mat2[0])):
@@ -17,5 +15,4 @@ def mat_mul(mat1, mat2):
                 sum += mat1[i][k] * mat2[k][j]
             row.append(sum)
         res.append(row)
-
     return res
