80f5c53195cad7eff605ea4fd8f8065e823fc3fe
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-11-30 01:13:08 -0300

    Added comments

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 def matrix_transpose(matrix):
+    """Retunrs the transpose of a matrix"""
     transpose = []
     for i in range(len(matrix[0])):
         transpose.append([])
