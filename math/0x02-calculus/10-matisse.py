40b05c0687c2abcd9d463b6674f25abda54ab795
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-08 17:24:00 -0300

    Fix task 10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,12 +6,11 @@ def poly_derivative(poly):
     """calculates the derivative of a polynomial"""
     if not poly or not isinstance(poly, list):
         return None
+    if len(poly) == 1:
+        return [0]
 
     derivative = []
-    for coeff in range(len(poly), 1, -1):
-        derivative.append((coeff - 1) * poly[coeff - 1])
-
-    if all(x == 0 for x in derivative):
-        return [0]
+    for coeff in range(1, len(poly)):
+        derivative.append((coeff) * poly[coeff])
 
     return derivative
