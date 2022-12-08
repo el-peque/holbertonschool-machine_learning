a64c2f022599c7dea218b4da0d193fa5f0897f05
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-08 19:06:03 -0300

    Fix task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,8 +5,9 @@
 def summation_i_squared(n):
     """calculates the sum of the squares of
     the first n positive integers"""
-    if not n or not isinstance(n, int) or n < 0:
+    if not isinstance(n, int) or n < 1:
         return None
     if n == 1:
         return 1
-    return (n**2 + summation_i_squared(n - 1))
+    sum = (n**2 + summation_i_squared(n - 1))
+    return sum
