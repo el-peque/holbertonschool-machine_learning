577eac4607f26779bee0d59df603e0923d178315
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-10 19:16:55 -0300

    Reverted changes

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 """summation_i_squared"""
-import sys
 
 
 def summation_i_squared(n):
     """calculates the sum of the squares of
     the first n positive integers"""
-    sys.setrecursionlimit(10000)
-
     if not isinstance(n, int) or n < 1:
         return None
     if n == 1:
