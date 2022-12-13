2c718af400146cb35734b58934bce62a7e3ac831
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-13 19:21:29 -0300

    Add task 1

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -4,6 +4,9 @@
 
 class Poisson:
     """Represents a poisson distribution"""
+    π = 3.1415926536
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         """Initiates poisson"""
         if data is None:
@@ -13,6 +16,14 @@ class Poisson:
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
-            if len(set(data)) < 2:
+            if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / len(data)
+
+    def pmf(self, k):
+        """Calculates the value of the PMF
+        for a given number of 'successes'"""
+        from math import factorial
+        k = int(k)
+        λ = self.lambtha
+        return ((self.e ** (-λ)) * (λ ** k) / factorial(k))
