effd6f720e195c5bbdb572b4187c6912a696a813
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-13 19:05:03 -0300

    Add task 0

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""class Poisson"""
+
+
+class Poisson():
+    """Represents a poisson distribution"""
+    def __init__(self, data=None, lambtha=1.):
+        """Initiates poisson"""
+        if data is None:
+            self.lambtha = lambtha
+        elif lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data) / len(data)
