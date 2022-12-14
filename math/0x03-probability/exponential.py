fb7d04a06aa8fd87acaf2e53c99da732061803b7
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-14 03:04:16 -0300

    Small fix

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -21,16 +21,14 @@ class Exponential:
 
     def pdf(self, x):
         """Calculates the value of the PDF for a given time period"""
-        x = int(x)
         if x < 0:
             return 0
 
         return (self.lambtha * ((self.e ** ((-self.lambtha) * x))))
 
-    def cdf(self, k):
+    def cdf(self, x):
         """Calculates the value of the CDF for a given time period"""
-        k = int(k)
-        if k < 0:
+        if x < 0:
             return 0
 
-        return (1 - (self.e ** (-self.lambtha * k)))
+        return (1 - (self.e ** (-self.lambtha * x)))
