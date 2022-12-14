a84666b6f1a7a21813b50ed8ed9404c90e94f966
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-14 03:01:33 -0300

    Add tasks 4 and 5

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -4,7 +4,6 @@
 
 class Exponential:
     """Represents an exponential distribution"""
-    π = 3.1415926536
     e = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
@@ -20,28 +19,18 @@ class Exponential:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(1 / (sum(data) / len(data)))
 
-    def pmf(self, k):
-        """Calculates the value of the PMF
-        for a given number of 'successes'"""
-        k = int(k)
-        if k < 0:
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given time period"""
+        x = int(x)
+        if x < 0:
             return 0
 
-        λ = self.lambtha
-        return ((self.e ** (-λ)) * (λ ** k) / Poisson.factorial(k))
+        return (self.lambtha * ((self.e ** ((-self.lambtha) * x))))
 
     def cdf(self, k):
-        """Calculates the value of the CDF for a given number of “successes”"""
+        """Calculates the value of the CDF for a given time period"""
         k = int(k)
         if k < 0:
             return 0
-        cdf = sum([self.pmf(i) for i in range(k + 1)])
-        return cdf
 
-    @staticmethod
-    def factorial(n):
-        """Calculates the factorial of a number"""
-        if n <= 1:
-            return 1
-        else:
-            return (n * Poisson.factorial(n - 1))
+        return (1 - (self.e ** (-self.lambtha * k)))
