b571b5c4f638745a86ad90b2edf1378e92a00812
Peque <ezequielcabrera2601@gmail.com>
2022-12-19 16:25:23 -0300

    Add task 5

---
+++ supervised_learning/0x01-classification/5-neuron.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""Class Neuron"""
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """nx is the number of input features to the neuron"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter method for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter method for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter method for A"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        z = (np.dot(self.W, X) + self.b)
+        self.__A = 1/(1 + np.exp(-z))
+        return self.A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        m = Y.shape[1]
+        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron’s predictions"""
+        A = np.where(self.forward_prop(X) >= 0.5, 1, 0)
+        cost = self.cost(Y, self.forward_prop(X))
+        return A, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """Calculates one pass of gradient descent on the neuron"""
