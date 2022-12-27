d20831605d53827e48794b78c8adde2e831cb9e9
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-27 13:59:56 -0300

    Small fix

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -7,9 +7,9 @@ class DeepNeuralNetwork:
     """Defines a deep neural network performing binary classification"""
     def __init__(self, nx, layers):
         """
-        -nx is the number of input features
-        -layers is a list representing the number of
-         nodes in each layer of the network
+        nx is the number of input features
+        layers is a list representing the number of
+        nodes in each layer of the network
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -17,8 +17,8 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-#        if not all(isinstance(i, int) and i > 0 for i in layers):
-#            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(i, int) and i > 0 for i in layers):
+            raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
