3db2c2a3ca2b1097c90cf6cd778b3cc1aba6531e
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-27 16:37:07 -0300

    Fix task 17

--- supervised_learning/0x01-classification/17-deep_neural_network.py
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -11,26 +11,27 @@ class DeepNeuralNetwork:
         -layers is a list representing the number of
          nodes in each layer of the network
         """
-        if not isinstance(nx, int):
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or layers == []:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        self.__L = len(layers)
-        self.__cache = {}
-        self.__weights = {}
-        for layer in range(len(layers)):
-            if layer[layer] < 1:
+        self.L = len(layers)
+        self.cache = {}
+        weights = {}
+        for i in range(len(layers)):
+            if layers[i] < 1:
                 raise TypeError("layers must be a list of positive integers")
-            b = np.zeros((layers[layer], 1))
-            self.__weights.update({f"b{layer + 1}": b})
-            if layer == 0:
-                weight = np.random.randn(layers[layer], nx) * np.sqrt(2 / nx)
+            b = np.zeros((layers[i], 1))
+            if i == 0:
+                weight = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
             else:
-                weight = np.random.randn(layers[layer], layers[layer-1]) *\
-                         np.sqrt(2/layers[layer-1])
-            self.__weights.update({f"W{layer + 1}": weight})
+                f1 = np.random.randn(layers[i], layers[i - 1])
+                f2 = np.sqrt(2 / layers[i - 1])
+                weight = f1 * f2
+            weights["W" + str(i + 1)] = weight
+            weights["b" + str(i + 1)] = b
 
     @property
     def L(self):
