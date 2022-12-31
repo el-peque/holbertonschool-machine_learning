456de7e6956fd7f1d18d9dcf2c3d1f27c3d1d9ee
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-12-31 13:36:08 -0300

    Try new fix

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -5,6 +5,10 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """converts a numeric label vector into a one-hot matrix"""
+    if not isinstance(Y, np.ndarray):
+        return None
+    if classes < np.amax(Y):
+        return None
     one_hot_m = np.zeros(shape=(len(Y), classes))
     for column, row in enumerate(Y):
         one_hot_m[column, row] = 1
