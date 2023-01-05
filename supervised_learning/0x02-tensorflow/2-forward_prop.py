a124271472d2781902f9e07ea89086b8dd395ba2
Peque <ezequielcabrera2601@gmail.com>
2023-01-05 17:54:54 -0300

    Add task 2

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import tensorflow.compat.v1 as tf
+create_layer = __import__('1-create_layer').create_layer
+"""Layers"""
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """Creates the forward propagation graph for the neural network"""
+    y = x
+    for size, activation in zip(layer_sizes, activations):
+        y = create_layer(y, size, activation)
+    return y
