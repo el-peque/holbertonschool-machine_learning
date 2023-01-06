9e05b57642488c0e893511eb883cfc3eaca304ac
Peque <ezequielcabrera2601@gmail.com>
2023-01-06 01:04:53 -0300

    Fix task 1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -6,6 +6,6 @@ import tensorflow.compat.v1 as tf
 def create_layer(prev, n, activation):
     """Returns: the tensor output of the layer"""
     initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.keras.layers.Dense(units=n, bias_initializer=initializer,
+    layer = tf.keras.layers.Dense(units=n, kernel_initializer=initializer,
                                   activation=activation, name='layer')(prev)
     return layer
