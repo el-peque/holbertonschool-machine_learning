1e1d64834ce0981e26c49573b674c1eb1f198043
Peque <ezequielcabrera2601@gmail.com>
2023-01-12 17:46:45 -0300

    Fix task 7

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -5,16 +5,17 @@ import tensorflow.compat.v1 as tf
 
 def evaluate(X, Y, save_path):
     """evaluates the output of a neural network"""
-    saver = tf.train.import_meta_graph(save_path + ".meta")
-
     with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + ".meta")
         saver.restore(sess, save_path)
-        x = tf.get_collection('x')[-1]
-        y = tf.get_collection('y')[-1]
-        y_pred = tf.get_collection('y_pred')[-1]
-        accuracy = tf.get_collection('accuracy')[-1]
-        loss = tf.get_collection('loss')[-1]
-        loss_value = sess.run(loss, feed_dict={x: X, y: Y})
-        accuracy_value = sess.run(accuracy, feed_dict={x: X, y: Y})
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+
         pred = sess.run(y_pred, feed_dict={x: X, y: Y})
-    return pred, accuracy_value, loss_value
+        accuracy_value = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss_value = sess.run(loss, feed_dict={x: X, y: Y})
+
+        return pred, accuracy_value, loss_value
