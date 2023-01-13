f256b7098f100814ef160b264493b91b9c76ca6b
Peque <ezequielcabrera2601@gmail.com>
2023-01-13 06:49:17 -0300

    Try new fix

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -5,5 +5,5 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """Updates the learning rate using inverse time decay in numpy"""
-    alpha = (1 / (1 + decay_rate * np.floor(global_step / decay_step))) * alpha
+    alpha = (1 / (1 + decay_rate * np.round(global_step / decay_step))) * alpha
     return alpha
