defd6783ddeb3860f4c743a16fb3290f6cf30c49
Peque <ezequielcabrera2601@gmail.com>
2023-01-13 06:47:20 -0300

    Add task 11

---
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Learning Rate Decay"""
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """Updates the learning rate using inverse time decay in numpy"""
+    alpha = (1 / (1 + decay_rate * np.floor(global_step / decay_step))) * alpha
+    return alpha
