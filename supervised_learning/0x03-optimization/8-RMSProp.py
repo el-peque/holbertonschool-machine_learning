797273b42ab77cf7ad6c29cf08696a1968e5012a
Peque <ezequielcabrera2601@gmail.com>
2023-01-13 06:14:31 -0300

    Add task 8

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""RMSProp Upgraded"""
+import tensorflow.compat.v1 as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """Creates the training operation for a neural network
+    in tensorflow using the RMSProp optimization algorithm"""
+    print("Hello")
+    train_op = tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                         decay=beta2,
+                                         epsilon=epsilon)
+    return train_op.minimize(loss)
