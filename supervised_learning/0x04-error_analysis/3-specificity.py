3d10caa653d982f6d01e5e4562e855e6b50e2374
Peque <ezequielcabrera2601@gmail.com>
2023-01-17 21:13:46 -0300

    Fix task 3

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -7,12 +7,12 @@ def specificity(confusion):
     """Calculates the specificity for each class in a confusion matrix"""
     classes = confusion.shape[0]
     specificity = np.zeros(classes)
-    conf = confusion
+    conf = confusion.T
     for i in range(classes):
-        tp = confusion[i, i]
+        tp = conf[i, i]
         tn = np.sum(np.sum(conf[:i, :i]) + np.sum(conf[:i, i+1:]) +
                     np.sum(conf[i+1:, :i]) + np.sum(conf[i+1:, i+1:]))
-        fp = np.sum(confusion[i, :]) - tp
+        fp = np.sum(conf[i, :]) - tp
         specificity[i] = tn / (tn + fp)
 
     return specificity
