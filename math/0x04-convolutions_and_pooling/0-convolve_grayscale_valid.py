73a686f85d306acdc96be0242a8da8304b2616e7
Peque <ezequielcabrera2601@gmail.com>
2023-01-19 20:29:37 -0300

    Add task 0

---
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Valid Convolution"""
+import numpy as np
+import time
+
+
+def convolve_grayscale_valid(images, kernel):
+    """Performs a valid convolution on grayscale images"""
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    output = np.zeros(shape=(m, h - kh + 1, w - kw + 1))
+    for i in range(output.shape[1]):
+        for j in range(output.shape[2]):
+            output[:, i, j] = np.sum(images[:, i:i+kh, j:j+kw] * kernel,
+                                     axis=(1, 2))
+    return output
