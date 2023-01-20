159b3692b66e58089a9d2f5eb8154f4d5bf21286
Peque <ezequielcabrera2601@gmail.com>
2023-01-20 17:55:55 -0300

    Add task 1

---
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Gradient Descent with L2 Regularization"""
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """Updates the weights and biases of a neural network using
+    gradient descent with L2 regularization"""
+    m = Y.shape[1]
+    for i in range(L, 0, -1):
+        A = cache['A' + str(i)]
+        W = weights['W' + str(i)]
+        if i == L:
+            dz = A - Y
+        else:
+            W2 = weights['W' + str(i + 1)]
+            dz = np.multiply(np.matmul(W2.T, dz), 1 - (A ** 2))
+        dW = (np.matmul(dz, cache['A' + str(i - 1)].T) + lambtha * W) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
+        weights['W' + str(i)] = W - alpha * dW
+        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
