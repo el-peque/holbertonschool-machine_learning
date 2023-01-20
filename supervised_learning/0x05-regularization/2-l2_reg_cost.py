d932bfd9a7c3a631d36e3b79320f0cdb1ca19772
Peque <ezequielcabrera2601@gmail.com>
2023-01-20 20:56:13 -0300

    Add task 2

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -5,4 +5,4 @@ import tensorflow.compat.v1 as tf
 
 def l2_reg_cost(cost):
     """Calculates the cost of a neural network with L2 regularization"""
-    return cost + tf.losses.get_regularization_loss()
+    return cost + tf.losses.get_regularization_losses()
