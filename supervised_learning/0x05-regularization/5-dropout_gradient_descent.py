b7eb6ab1f481a0630b15952f6f60f6b4e588cf2e
Peque <ezequielcabrera2601@gmail.com>
2023-01-22 20:43:03 -0300

    Add task 5

---
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Gradient Descent with Dropout"""
+import numpy as np
+
+
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    """Updates the weights of a neural network with
+    Dropout regularization using gradient descent"""
+    m = Y.shape[1]
+    w = weights.copy()
+    for i in range(L, 0, -1):
+        A = cache['A' + str(i)]
+        if i == L:
+            dz = A - Y
+        else:
+            dz = (np.matmul((w['W' + str(i + 1)]).T, dz)) * (1 - (A ** 2))
+            D = cache['D' + str(i)]
+            dz = (dz * D) / keep_prob
+        dW = np.matmul(dz, cache['A' + str(i - 1)].T) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
+        weights['W' + str(i)] = w['W' + str(i)] - alpha * dW
+        weights['b' + str(i)] = w['b' + str(i)] - alpha * db
