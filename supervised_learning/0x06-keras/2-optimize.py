2f0e57f90641f331600f94468b95d03c1db59e24
Peque <ezequielcabrera2601@gmail.com>
2023-01-24 16:40:05 -0300

    Fix tas 2

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -9,5 +9,7 @@ def optimize_model(network, alpha, beta1, beta2):
     opt = K.optimizers.Adam(learning_rate=alpha,
                             beta_1=beta1,
                             beta_2=beta2)
-    network.compile(loss='categorical_crossentropy', optimizer=opt)
+    network.compile(loss='categorical_crossentropy',
+                    optimizer=opt,
+                    metrics='accuracy')
     return
