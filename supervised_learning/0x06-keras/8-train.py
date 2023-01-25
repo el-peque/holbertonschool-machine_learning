7d6273d09f855ce5a9e4a8bc20682f66798d56a0
Peque <ezequielcabrera2601@gmail.com>
2023-01-25 11:26:24 -0300

    Add task 8

---
+++ supervised_learning/0x06-keras/8-train.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Save Only the Best"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, learning_rate_decay=False,
+                alpha=0.1, decay_rate=1, save_best=False,
+                filepath=None, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent, early stopping,
+    learning rate decay and saving the best iteration of the model
+    """
+    callback = []
+    if validation_data and learning_rate_decay:
+        def lr_scheduler(epoch):
+            """Learning rate scheduler"""
+            return alpha / (1 + decay_rate * epoch)
+
+        callback.append(K.callbacks.LearningRateScheduler(lr_scheduler,
+                                                          verbose=1))
+
+    if validation_data and early_stopping:
+        callback.append(K.callbacks.EarlyStopping(monitor="val_loss",
+                                                  mode="min",
+                                                  patience=patience))
+
+    if save_best:
+        callback.append(K.callbacks.ModelCheckpoint(filepath,
+                                                    monitor="val_loss",
+                                                    save_best_only=True))
+
+    history = network.fit(data,
+                          labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          callbacks=callback,
+                          shuffle=shuffle,
+                          validation_data=validation_data)
+    return history
