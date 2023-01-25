7bd2ee7958b27faf7551eb6d55b4e6d7d72bd861
Peque <ezequielcabrera2601@gmail.com>
2023-01-25 11:08:33 -0300

    Fix task 6

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -7,16 +7,17 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
     """Trains a model using mini-batch gradient descent and early stopping"""
-    callback = None
+    callback = []
     if validation_data and early_stopping:
-        callback = K.callbacks.EarlyStopping(monitor="val_loss",
-                                             patience=patience)
+        callback.append(K.callbacks.EarlyStopping(monitor="val_loss",
+                                                  mode="min",
+                                                  patience=patience))
     history = network.fit(data,
                           labels,
                           batch_size=batch_size,
                           epochs=epochs,
                           verbose=verbose,
-                          callbacks=[callback],
+                          callbacks=callback,
                           shuffle=shuffle,
                           validation_data=validation_data)
     return history
