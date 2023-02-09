d31a91b40c5057de34e39497fcb64ebbfd061a3c
Peque <ezequielcabrera2601@gmail.com>
2023-02-09 00:26:21 -0300

    Fix task 2

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -10,26 +10,23 @@ def identity_block(A_prev, filters):
     """
     conv_1 = K.layers.Conv2D(filters=filters[0],
                              kernel_size=(1, 1),
-                             activation='relu',
                              padding='same',
                              kernel_initializer='he_normal')(A_prev)
-    batch_norm_1 = K.layers.BatchNormalization(axis=3)(conv_1)
+    batch_norm_1 = K.layers.BatchNormalization()(conv_1)
     relu_1 = K.layers.Activation(activation='relu')(batch_norm_1)
 
     conv_2 = K.layers.Conv2D(filters=filters[1],
                              kernel_size=(3, 3),
-                             activation='relu',
                              padding='same',
                              kernel_initializer='he_normal')(relu_1)
-    batch_norm_2 = K.layers.BatchNormalization(axis=3)(conv_2)
+    batch_norm_2 = K.layers.BatchNormalization()(conv_2)
     relu_2 = K.layers.Activation(activation='relu')(batch_norm_2)
 
     conv_3 = K.layers.Conv2D(filters=filters[2],
                              kernel_size=(1, 1),
-                             activation='relu',
                              padding='same',
                              kernel_initializer='he_normal')(relu_2)
-    batch_norm_3 = K.layers.BatchNormalization(axis=3)(conv_3)
+    batch_norm_3 = K.layers.BatchNormalization()(conv_3)
 
     add = K.layers.Add()([batch_norm_3, A_prev])
     relu_3 = K.layers.Activation(activation='relu')(add)
