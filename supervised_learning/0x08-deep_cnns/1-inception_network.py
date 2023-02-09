d98fb376cd51dcc370755b3b46ae2ce7625e552c
Peque <ezequielcabrera2601@gmail.com>
2023-02-09 00:18:57 -0300

    Fix task 1

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -25,26 +25,53 @@ def inception_network():
             'kernel_initializer': 'he_normal'}
 
     inputs = K.Input(shape=(224, 224, 3))
-    lay_1 = K.layers.Conv2D(**conv_1)(inputs)
-    lay_2 = K.layers.MaxPooling2D(**maxpool_1)(lay_1)
-    lay_3 = K.layers.Conv2D(**conv_2)(lay_2)
-    lay_4 = K.layers.Conv2D(**conv_3)(lay_3)
-    lay_5 = K.layers.MaxPooling2D(**maxpool_1)(lay_4)
-    lay_6 = inception_block(lay_5, (64, 96, 128, 16, 32, 32))
-    lay_7 = inception_block(lay_6, (128, 128, 192, 32, 96, 64))
-    lay_8 = K.layers.MaxPooling2D(**maxpool_1)(lay_7)
-    lay_9 = inception_block(lay_8, (192, 96, 208, 16, 48, 64))
-    lay_10 = inception_block(lay_9, (160, 112, 224, 24, 64, 64))
-    lay_11 = inception_block(lay_10, (128, 128, 256, 24, 64, 64))
-    lay_12 = inception_block(lay_11, (112, 144, 288, 32, 64, 64))
-    lay_13 = inception_block(lay_12, (256, 160, 320, 32, 128, 128))
-    lay_14 = K.layers.MaxPooling2D(**maxpool_1)(lay_13)
-    lay_15 = inception_block(lay_14, (256, 160, 320, 32, 128, 128))
-    lay_16 = inception_block(lay_15, (384, 192, 384, 48, 128, 128))
-    lay_17 = K.layers.AveragePooling2D(**avgpool_1)(lay_16)
-    lay_18 = K.layers.Dropout(rate=(0.4))(lay_17)
-    lay_19 = K.layers.Dense(**fc_1)(lay_18)
-    model = K.Model(inputs=inputs, outputs=lay_19)
+    conv_1 = K.layers.Conv2D(filters=64,
+                             kernel_size=(7, 7),
+                             strides=2,
+                             activation='relu',
+                             padding='same',
+                             kernel_initializer='he_normal')(inputs)
+    maxpool_1 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                      strides=2,
+                                      padding='same')(conv_1)
+    conv_2 = K.layers.Conv2D(filters=64,
+                             kernel_size=(1, 1),
+                             strides=1,
+                             activation='relu',
+                             padding='valid',
+                             kernel_initializer='he_normal')(maxpool_1)
+    conv_3 = K.layers.Conv2D(filters=192,
+                             kernel_size=(3, 3),
+                             strides=1,
+                             activation='relu',
+                             padding='same',
+                             kernel_initializer='he_normal')(conv_2)
+    maxpool_2 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                      strides=2,
+                                      padding='same')(conv_3)
+    inception_1 = inception_block(maxpool_2, (64, 96, 128, 16, 32, 32))
+    inception_2 = inception_block(inception_1, (128, 128, 192, 32, 96, 64))
+    maxpool_3 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                      strides=2,
+                                      padding='same')(inception_2)
+    inception_3 = inception_block(maxpool_3, (192, 96, 208, 16, 48, 64))
+    inception_4 = inception_block(inception_3, (160, 112, 224, 24, 64, 64))
+    inception_5 = inception_block(inception_4, (128, 128, 256, 24, 64, 64))
+    inception_6 = inception_block(inception_5, (112, 144, 288, 32, 64, 64))
+    inception_7 = inception_block(inception_6, (256, 160, 320, 32, 128, 128))
+    maxpool_4 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                      strides=2,
+                                      padding='same')(inception_7)
+    inception_8 = inception_block(maxpool_4, (256, 160, 320, 32, 128, 128))
+    inception_9 = inception_block(inception_8, (384, 192, 384, 48, 128, 128))
+    avgpool = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                        strides=1,
+                                        padding='valid')(inception_9)
+    dropout = K.layers.Dropout(rate=(0.4))(avgpool)
+    fc = K.layers.Dense(units=(1000),
+                        activation='softmax',
+                        kernel_initializer='he_normal')(dropout)
+    model = K.Model(inputs=inputs, outputs=fc)
     model.compile(optimizer=K.optimizers.Adam(),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
