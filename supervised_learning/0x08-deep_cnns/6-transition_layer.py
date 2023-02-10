961a43bdac8f1f7b0c182f39c1fc5cbe15df0145
Peque <ezequielcabrera2601@gmail.com>
2023-02-10 19:33:53 -0300

    Add task 6

---
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Transition Layer"""
+import tensorflow.keras as K
+
+def transition_layer(X, nb_filters, compression):
+    """
+    Builds a transition layer as described in
+    'Densely Connected Convolutional Networks'
+    """
+    batch_norm = K.layers.BatchNormalization()(X)
+    nb_filters = int(nb_filters*compression)
+    conv = K.layers.Conv2D(filters=nb_filters,
+                           kernel_size=(1, 1),
+                           padding='same',
+                           kernel_initializer='he_normal')(batch_norm)
+    avgpool = K.layers.AveragePooling2D(pool_size=(2, 2),
+                                        strides=(2, 2),
+                                        padding='same')(conv)
+    return avgpool, nb_filters
