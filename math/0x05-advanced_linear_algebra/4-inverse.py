ddc7c4f252d903c6acd44ab71831b48fbec9644e
Peque <ezequielcabrera2601@gmail.com>
2023-03-09 12:59:59 -0300

    Add task 4

---
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Inverse"""
+
+
+def inverse(matrix):
+    """Calculates the inverse of a matrix"""
+    determinant = __import__('0-determinant').determinant
+    adjugate = __import__('3-adjugate').adjugate
+    m_determinant = determinant(matrix)
+    m_adjugate = adjugate(matrix)
+    if m_determinant == 0:
+        return
+    m_inverse = []
+    for i in range(len(matrix)):
+        row = []
+        for j in range(len(matrix[i])):
+            row.append(m_adjugate[i][j] / m_determinant)
+        m_inverse.append(row)
+    return m_inverse
