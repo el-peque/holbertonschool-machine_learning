dda5fd6d30f117e52965d5d45b29751e97cd4ae1
Peque <ezequielcabrera2601@gmail.com>
2023-03-13 14:33:36 -0300

    Update 1-correlation.py

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -7,11 +7,8 @@ def correlation(C):
     """Calculates a correlation matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a 2D numpy.ndarray")
-    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+    if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
-    stdev = (np.diag(C) ** 0.5)
-    D = np.diag(1 / stdev)
-    M = np.dot(np.dot(D, C), D)
-    R = np.diag(1 / (np.diag(M) ** 0.5))
-    corr_M = np.dot(np.dot(R, M), R)
+    D = np.diag(1 / (np.diag(C) ** 0.5))
+    corr_M = np.dot(np.dot(D, C), D)
     return corr_M
