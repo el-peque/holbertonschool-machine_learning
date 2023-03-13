939c0b506f8ca972873c77205d753f3a2bd6a740
Peque <ezequielcabrera2601@gmail.com>
2023-03-13 14:44:48 -0300

    Create multinormal.py

---
+++ math/0x06-multivariate_prob/multinormal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Initialize"""
+import numpy as np
+
+
+class MultiNormal:
+    """Represents a Multivariate Normal distribution"""
+    def __init__(self, data):
+        """Instanciates class"""
+        mean_cov = __import__('0-mean_cov').mean_cov
+
+        if not isinstance(data, np.ndarray) or data.ndim != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        d, n = data.shape
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+        self.mean, self.cov = mean_cov(data)
