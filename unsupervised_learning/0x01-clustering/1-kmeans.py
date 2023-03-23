04b1b1812c7db28eec03f654e3432e82ba369c7e
Peque <ezequielcabrera2601@gmail.com>
2023-03-23 20:57:01 -0300

    Fix task 1

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -8,22 +8,19 @@ def kmeans(X, k, iterations=1000):
     if not isinstance(X, np.ndarray) or not isinstance(k, int)\
        or len(X.shape) != 2 or X.shape[0] < k or k <= 0 or iterations <= 0:
         return None
-    min_x = np.min(X, axis=0)
-    max_x = np.max(X, axis=0)
-    C = np.random.uniform(min_x, max_x, size=(k, X.shape[1]))
+    min_vals = np.min(X, axis=0)
+    max_vals = np.max(X, axis=0)
+    C = np.random.uniform(low=min_vals, high=max_vals, size=(k, X.shape[1]))
     clss = np.zeros(X.shape[0],)
-
     for i in range(iterations):
         distances = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=2))
         clss = np.argmin(distances, axis=0)
-        prev_C = C.copy()
+        old_C = np.copy(C)
         for j in range(k):
             if np.sum(clss == j) == 0:
-                C[j] = np.random.uniform(min_x, max_x, size=(1, X.shape[1]))
+                C[j] = np.random.uniform(low=min_vals, high=max_vals, size=(1, X.shape[1]))
             else:
                 C[j] = np.mean(X[clss == j], axis=0)
-
-        if np.allclose(C, prev_C):
+        if np.allclose(C, old_C):
             return C, clss
-
     return None, None
