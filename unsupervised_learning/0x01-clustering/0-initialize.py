72d40bc7261625741a391798af715ae42413b586
Peque <ezequielcabrera2601@gmail.com>
2023-03-23 20:33:18 -0300

    Fix task 0

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -6,7 +6,7 @@ import numpy as np
 def initialize(X, k):
     """Initializes cluster centroids for K-means"""
     if not isinstance(X, np.ndarray) or not isinstance(k, int)\
-       or len(X.shape) != 2:
+       or len(X.shape) != 2 or X.shape[0] < k:
         return None
     min_x = np.min(X, axis=0)
     max_x = np.max(X, axis=0)
