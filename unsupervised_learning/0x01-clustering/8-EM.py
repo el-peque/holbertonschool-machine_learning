17dc677c1724370290ec81c0a055c153f91389ad
Peque <ezequielcabrera2601@gmail.com>
2023-03-23 22:21:34 -0300

    Create 8-EM.py

---
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Expectation Maximization"""
+import numpy as np
+
+
+def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
+    """Performs the expectation maximization for a GMM"""
+    return None, None, None, None, None
