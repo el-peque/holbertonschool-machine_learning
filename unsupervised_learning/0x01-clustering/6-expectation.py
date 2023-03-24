7c29046e180403f39702fc241197f9d3d9710c43
Peque <ezequielcabrera2601@gmail.com>
2023-03-23 22:08:02 -0300

    Fix task 6

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -6,6 +6,13 @@ pdf = __import__('5-pdf').pdf
 
 def expectation(X, pi, m, S):
     """Calculates the expectation step in the EM algorithm for a GMM"""
+    if not isinstance(X, np.ndarray) or not isinstance(m, np.ndarray)\
+       or not isinstance(S, np.ndarray) or not isinstance(pi, np.ndarray)\
+       or len(X.shape) != 2 or len(pi.shape) != 1 or len(m.shape) != 2\
+       or len(S.shape) != 3\
+       or not (X.shape[1] == m.shape[1] == S.shape[1] == S.shape[2])\
+       or not (pi.shape[0] == m.shape[0] == S.shape[0]):
+        return None, None
     try:
         dens = np.zeros((pi.shape[0], X.shape[0]))
         for i in range(pi.shape[0]):
