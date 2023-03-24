c4c3eb4758dab923c6bfe2e461088573d3e6f93b
Peque <ezequielcabrera2601@gmail.com>
2023-03-23 22:28:31 -0300

    Update

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Bayesian Information Criterion"""
 import numpy as np
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """Finds the best number of clusters for a GMM using the Bayesian Information Criterion"""
+    """Finds the best number of clusters for a GMM using
+    the Bayesian Information Criterion"""
     return None, None, None, None
