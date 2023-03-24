c61625e111bfaf1e34a6d5788e0a3ed930ab1282
Peque <ezequielcabrera2601@gmail.com>
2023-03-23 21:59:35 -0300

    Fix task 5

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -7,7 +7,7 @@ def pdf(X, m, S):
     """Calculates the PDF of a Gaussian distribution"""
     if not isinstance(X, np.ndarray) or not isinstance(m, np.ndarray)\
        or not isinstance(S, np.ndarray) or len(X.shape) != 2 or\
-       len(m.shape[0]) != 1 or len(S.shape) != 2 or\
+       len(m.shape) != 1 or len(S.shape) != 2 or\
        not (X.shape[1] == m.shape[0] == S.shape[0] == S.shape[1]):
         None
     d = m.shape[0]
