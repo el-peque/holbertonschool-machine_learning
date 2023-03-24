e129d51b42320662e9955d24b2f82cbef4c379da
Peque <ezequielcabrera2601@gmail.com>
2023-03-23 22:00:35 -0300

    Fix task 5

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -9,10 +9,13 @@ def pdf(X, m, S):
        or not isinstance(S, np.ndarray) or len(X.shape) != 2 or\
        len(m.shape) != 1 or len(S.shape) != 2 or\
        not (X.shape[1] == m.shape[0] == S.shape[0] == S.shape[1]):
-        None
-    d = m.shape[0]
-    X = X - m.reshape(1, d)
-    P = np.exp(-0.5 * np.sum(X @ np.linalg.inv(S) * X, axis=1)) /\
-        np.sqrt((2 * np.pi)**d * np.linalg.det(S))
-    P = np.maximum(P, 1e-300)
-    return P
+        return None
+    try:
+        d = m.shape[0]
+        X = X - m.reshape(1, d)
+        P = np.exp(-0.5 * np.sum(X @ np.linalg.inv(S) * X, axis=1)) /\
+            np.sqrt((2 * np.pi)**d * np.linalg.det(S))
+        P = np.maximum(P, 1e-300)
+        return P
+    except Exception:
+        return None
