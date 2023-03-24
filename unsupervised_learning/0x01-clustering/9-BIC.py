c21f7ebb605cb4e79bc0f2784441dc6edebbe0ff
Peque <ezequielcabrera2601@gmail.com>
2023-03-23 22:23:12 -0300

    Create 9-BIC.py

---
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Bayesian Information Criterion"""
+import numpy as np
+
+
+def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """Finds the best number of clusters for a GMM using the Bayesian Information Criterion"""
+    return None, None, None, None
