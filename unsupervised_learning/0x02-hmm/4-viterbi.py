f207c12e204a159a2d12d3c140212a52ef129d7e
Peque <ezequielcabrera2601@gmail.com>
2023-03-30 21:54:33 -0300

    Create 4-viterbi.py

---
+++ unsupervised_learning/0x02-hmm/4-viterbi.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""The Viretbi Algorithm"""
+import numpy as np
+
+
+def viterbi(Observation, Emission, Transition, Initial):
+    """Calculates the most likely sequence of hidden states
+    for a hidden markov model"""
+    return None, None
