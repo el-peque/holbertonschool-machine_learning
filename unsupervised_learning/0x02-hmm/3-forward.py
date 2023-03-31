311de8fbd9a8becfea27db68575f30d028f24101
Peque <ezequielcabrera2601@gmail.com>
2023-03-30 21:53:08 -0300

    Create 3-forward.py

---
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""The Forward Algorithm"""
+import numpy as np
+
+
+def forward(Observation, Emission, Transition, Initial):
+    """Performs the forward algorithm for a hidden markov model"""
+    return None, None
