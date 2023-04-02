f4c3241b117d458a5bf9568c48bc8be513a8b860
Peque <ezequielcabrera2601@gmail.com>
2023-04-02 20:39:36 -0300

    Create 6-baum_welch.py

---
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""The Baum-Welch Algorithm"""
+import numpy as np
+
+
+def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
+    """Performs the Baum-Welch algorithm for a hidden markov model"""
+    return None, None
