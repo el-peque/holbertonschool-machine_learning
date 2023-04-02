0311d76fa811404f8626234879cd331aafaf9203
Peque <ezequielcabrera2601@gmail.com>
2023-04-02 20:38:00 -0300

    Create 5-backward.py

---
+++ unsupervised_learning/0x02-hmm/5-backward.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""The Backward Algorithm"""
+import numpy as np
+
+
+def backward(Observation, Emission, Transition, Initial):
+    """Performs the backward algorithm for a hidden markov model"""
+    return None, None
