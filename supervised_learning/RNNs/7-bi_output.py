e8cdded6035e23362b2db23227362a141604905c
Peque <ezequielcabrera2601@gmail.com>
2023-09-05 15:21:02 -0300

    Add task 7

---
+++ supervised_learning/RNNs/7-bi_output.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Bidirectional Output"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """Represents a bidirectional cell of an RNN"""
+
+    def __init__(self, i, h, o):
+        """Class constructor"""
+        self.Whf = np.random.normal(size=(i + h, h))
+        self.bhf = np.zeros((1, h))
+
+        self.Whb = np.random.normal(size=(i + h, h))
+        self.bhb = np.zeros((1, h))
+
+        self.Wy = np.random.normal(size=(2 * h, o))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """Performs forward propagation for one time step"""
+        concat_input_f = np.hstack((h_prev, x_t))
+        h_next_f = np.tanh(np.dot(concat_input_f, self.Whf) + self.bhf)
+
+        return h_next_f
+
+    def backward(self, h_next, x_t):
+        """Performs backward propagation for one time step"""
+        concat_input_b = np.hstack((h_next, x_t))
+        h_prev_b = np.tanh(np.dot(concat_input_b, self.Whb) + self.bhb)
+
+        return h_prev_b
+
+    def output(self, H):
+        """Calculates all outputs for the RNN"""
+        Y = np.exp(np.dot(H, self.Wy) + self.by)
+        Y = Y / np.sum(Y, axis=2, keepdims=True)
+
+        return Y
